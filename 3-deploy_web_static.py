7b3044480537356b7579c77696bebe2abb136cc0
Joshua Baka <fatow.jay@gmail.com>
2025-02-16 01:11:41 +0000

    executable command

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Create and distributes an archive to web servers"""
+"""Create and distributes an archive to web servers
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+"""
 import os.path
 import time
 from fabric.api import local
